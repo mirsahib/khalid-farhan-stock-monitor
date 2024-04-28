2cbe1ed4f4206e74b4d6632708514dcec231fe67
Mir Sahib <mirsahib24@gmail.com>
2024-04-28 10:11:23 +0600

    :hammer: update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,30 +1,29 @@
 import React from "react";
+import { Github, LinkedinIcon } from "lucide-react";
 
 function Footer() {
     return (
         <footer className="text-gray-600 body-font">
             <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
                 <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-                    Logo
-                    <span className="ml-3 text-xl">Tailblocks</span>
+                    <a
+                        className="text-gray-500"
+                        href="https://github.com/mirsahib"
+                    >
+                        <Github />
+                    </a>
                 </a>
                 <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-                    © 2020 Tailblocks —
+                    © {new Date().getFullYear()} Made with &hearts; —
                     <a
-                        href="https://twitter.com/knyttneve"
+                        href="https://www.linkedin.com/in/mir-habib-ul-latif/"
                         className="text-gray-600 ml-1"
                         rel="noopener noreferrer"
                         target="_blank"
                     >
-                        @knyttneve
+                        Mir Habib Ul Latif
                     </a>
                 </p>
-                <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-                    <a className="text-gray-500">logo</a>
-                    <a className="ml-3 text-gray-500">logo</a>
-                    <a className="ml-3 text-gray-500">Another Logo</a>
-                    <a className="ml-3 text-gray-500">Logo</a>
-                </span>
             </div>
         </footer>
     );
